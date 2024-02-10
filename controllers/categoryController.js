4fe6fa5b13c1f3ed72c05ecf392f513f752f9eb5
Soniasalma <soniasalma93eskander@gmail.com>
2024-02-10 01:59:35 -0400

    add CRUD for category api

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -28,3 +28,81 @@ export const createCategoryController = async (req, res) => {
     });
   }
 };
+export const updateCategoryController = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { id } = req.params;
+    const category = await Category.findByIdAndUpdate(
+      id,
+      { name, slug: slugify(name) },
+      { new: true }
+    );
+    res.status(200).send({
+      success: true,
+      message: 'Category Updated Successfully',
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: 'Error while updating category',
+    });
+  }
+};
+
+//get all categories
+
+export const categoryController = async (req, res) => {
+  try {
+    const category = await Category.find({});
+    res.status(200).send({
+      success: true,
+      message: 'All Categories List',
+      category,
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      error,
+      message: 'Error while getting all categories',
+    });
+  }
+};
+//single category
+export const singleCategoryController = async (req, res) => {
+  try {
+    const category = Category.findOne({ slug: req.params.slug });
+    res.status(200).send({
+      success: true,
+      message: 'Get Single Category Successfully',
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: 'Error while getting single Category',
+    });
+  }
+};
+//delete category
+export const deleteCategoryController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Category.findByIdAndDelete(id);
+    res.status(200).send({
+      success: true,
+      message: 'Category Deleted Successfully',
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: 'Error while deleting Category',
+    });
+  }
+};
